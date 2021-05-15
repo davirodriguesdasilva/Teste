5a072d2ddf48b02bb2f1d4a978df2b93e856609e
Davi Rodrigues da Silva <davi.silva@maqina.com.br>
2021-05-15 01:57:33 -0300

    Header, carousel e terceiro bloco.

---
+++ index.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+  <link rel="stylesheet" href="css/style.css">
+  <title>Teste</title>
+</head>
+<body>
+  
+  <!-- ---------------------NAV BAR--------------------- -->
+  
+  <div id="navbar">
+    
+    <div class="container">
+      
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#">
+          <div class="logo-container">
+            <img src="assets/logo.jpg" alt="">
+          </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">EMPRESA</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">SERVIÇOS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">NOTÍCIAS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CONTATO</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+  
+  <!-- ---------------------HEADER--------------------- -->
+  
+  <header>
+    
+    <div class="container-fluid">
+      
+      <div id="headerCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#headerCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#headerCarousel" data-slide-to="1"></li>
+          <li data-target="#headerCarousel" data-slide-to="2"></li>
+          <li data-target="#headerCarousel" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active align-self-center">
+            <div class="text-container">
+              <h2>Prepare-se para<br/> muita adrenalina</h2>
+              <p>Oferecemos diversas opções para você se<br/> aventurar na neve</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="text-container">
+              <h2>Prepare-se para<br/> muita adrenalina</h2>
+              <p>Oferecemos diversas opções para você se<br/> aventurar na neve</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="text-container">
+              <h2>Prepare-se para<br/> muita adrenalina</h2>
+              <p>Oferecemos diversas opções para você se<br/> aventurar na neve</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="text-container">
+              <h2>Prepare-se para<br/> muita adrenalina</h2>
+              <p>Oferecemos diversas opções para você se<br/> aventurar na neve</p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#headerCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </a>
+        <a class="carousel-control-next" href="#headerCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </a>
+      </div>
+      
+    </div>
+    
+  </header>
+  
+  <!-- ---------------------QUEM SOMOS--------------------- -->
+  
+  <section class="bloco-2">
+    
+    <div class="container-fluid">
+      <div class="row">
+        
+        <div class="col-lg">
+          <div class="red-img"></div>
+        </div>
+        <div class="col-lg">
+          <div class="elements-container">
+            <h2>QUEM SOMOS</h2>
+            <P>Lorem ipsum dolor sit amet, 
+              consectetuer adipiscing elit, sed diam 
+              nonummy nibh euismod tincidunt ut laoreet dolore magna 
+              aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud 
+              exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea 
+              commodo consequat. Duis autem vel eum iriure dolor in hendrerit in 
+              vulputate velit esse molestie consequat, vel illum dolore eu feugiat 
+              nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit 
+              praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</P>
+            
+              <div class="cta-container">
+                <p>CONHEÇA MAIS SOBRE</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+
+    <!-- ---------------------BLOCO 3--------------------- -->
+
+    <section class="bloco-3">
+
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg">
+
+          </div>
+          <div class="col-lg">
+
+          </div>
+          <div class="col-lg">
+
+          </div>
+
+        </div>
+      </div>
+      
+    </section>
+    
+    
+    
+    <script src="main.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script> -->
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+  </body>
+  </html>
